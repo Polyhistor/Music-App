d6e228c56b58872a37312651ad4964665ad37a0f
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-10-06 11:57:49 +1300

    setup our routers

--- manifest.0d850ea08bf14b589c82.js
+++ manifest.0d850ea08bf14b589c82.js
@@ -80,7 +80,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + {"0":"fe9e18cc19051f99bcc0","1":"e0bf29dbd7c5a8ed2f0f"}[chunkId] + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + {"0":"fe9e18cc19051f99bcc0","1":"308600f3d0093b3825f5"}[chunkId] + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
